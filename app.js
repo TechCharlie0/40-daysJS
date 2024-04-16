8313040fc3c5f36222e09db8077574128c9f6044
isha kandel <ishachttri@gmail.com>
2024-04-16 09:41:45 +0545

    template-leteral

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-console.log("i am isha kandel");
-console.log("this is my first js class");
+
+// console log wala topic ho
+
+// console.log("i am isha kandel");
+// console.log("this is my first js class");
+// let a = 10;
+// let b = 20;
+// sum = a++;
+// console.log("sum:",a+b);
+// console.log("sum:",a++);
+// console.log("sum:",+a);
+
+
+// template leteral
+
+let pencilPrice = 10;
+let eraser = 20;
+// let output = "the total price:"+ (pencilPrice + eraser)+ "rupees.";
+// console.log(`the total price: ${pencilPrice} rupees.`); 
+console.log(`the total price: ${pencilPrice + eraser} rupees.`)
