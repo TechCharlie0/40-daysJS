3cf6042ffc9149e229014462bc3380d81777ef96
isha kandel <ishachttri@gmail.com>
2024-04-16 10:56:04 +0545

    marks js

--- app.js
+++ app.js
@@ -67,22 +67,178 @@
 // let age=(a != b);
 // console.log(age);
 
-let a = 'P';
-let  b= 'p';
-let unicode=(a < b);
-console.log(unicode);
+
+// comparasion of non number like unicode
+// let a = 'P';
+// let  b= 'p';
+// let unicode=(a < b);
+// console.log(unicode);
+
+
+
+// conditional statement
+
+
+// let age = 30;
+// if (age >= 20) {
+//     console.log("you can drive");
+// }
+// else {
+//     console.log("you cannot drive");
+    
+// }
+
+
+
+
+
+// traffic light color
+
+// let color = "red"
+
+// if (color === "red" ) {
+//     console.log("stop!");
+    
+// }
+// if (color ==="yellow") {
+//     console.log("slow down");
+    
+// }
+
+// if (color === "green") {
+//     console.log("go");
+// }
+
+
+
+
+// let color = "yellow"
+
+// if (color === "red" ) {
+//     console.log("stop!");
+    
+// }
+// if (color ==="yellow") {
+//     console.log("slow down");
+    
+// }
+
+// if (color === "green") {
+//     console.log("go");
+// }
+
+
+
+
+
+// let color = "green"
+
+// if (color === "red" ) {
+//     console.log("stop!");
+    
+// }
+// if (color ==="yellow") {
+//     console.log("slow down");
+    
+// }
+
+// if (color === "green") {
+//     console.log("go");
+// }
 
 
 
 
 
 
+// else if statement
+
+// let a = 40;
+// if (a >= 50) {
+//     console.log("you canot vote");
+    
+// }
+// else if(a < 50){
+//     console.log("you can vote");
+
+    
+// }
 
 
 
 
+// marks if and else if
+
+// let marks = 75;
+
+// if (marks >= 80) {
+//     console.log("A+");
+    
+// }
+// else if (marks >= 60){
+//     console.log("B+");
+// }
+// else if (marks >= 40){
+//     console.log("c+");
+// }
 
 
+// else-statement
+// let a = 70
+// if (a <30) {
+//     console.log("you can go")
+    
+// } else {
+//     console.log("you cannot go")
+// }
+
+
+
+let color = "yellow"
+if (color === "red") {
+    console.log("you can go");
+    
+}
+else if( color === "green"){
+    console.log("you cannot go");
+}
+
+else {
+    console.log("go");
+}
+
+
+let grade = 20;
+
+if (grade >= 90) {
+    console.log("Conguralation you get A+");
+    
+}
+
+ else if (grade >= 80) {
+    console.log("Conguralation you get A");
+    
+}
+ else if (grade >= 70) {
+    console.log("Conguralation you get B+");
+    
+}
+else if (grade >= 60) {
+    console.log("Conguralation you get B");
+    
+}
+ else if (grade >= 50) {
+    console.log("Conguralation you get C+");
+    
+}
+
+ else if (grade >= 40) {
+    console.log("Conguralation you get C");
+    
+}
+else{
+    console.log("Failed");
+}
 
 
 
