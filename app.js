76ec418401c19ddb433e0a178b7eb937753daaad
isha kandel <ishachttri@gmail.com>
2024-04-16 10:00:13 +0545

    operator

--- app.js
+++ app.js
@@ -13,8 +13,56 @@
 
 // template leteral
 
-let pencilPrice = 10;
-let eraser = 20;
+// let pencilPrice = 10;
+// let eraser = 20;
 // let output = "the total price:"+ (pencilPrice + eraser)+ "rupees.";
 // console.log(`the total price: ${pencilPrice} rupees.`); 
-console.log(`the total price: ${pencilPrice + eraser} rupees.`)
+// console.log(`the total price: ${pencilPrice + eraser} rupees.`)
+
+// artthemetic operator
+
+// let a = 20;
+// let b = 89;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// unary operator
+// let a = 30;
+// let b = 59;
+// console.log(a++);
+// console.log(++a);
+
+// assigment operator
+
+// let a = 10;
+// let b = 2;
+// let c = 20;
+// b = a;
+// a = c;
+// console.log(b);
+// console.log(a);
+
+// comparasion operator
+
+let a = 20;
+let  b= 50;
+let age=(a<b);
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
