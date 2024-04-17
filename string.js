9edb3b8c76fae9f5f06a5d124f3309e35962af06
isha kandel <ishachttri@gmail.com>
2024-04-17 11:48:19 +0545

    repeat function

--- string.js
+++ string.js
@@ -9,6 +9,23 @@
 
 // Develop a script that prompts the user to input a string. Use the `toLowerCase()` and `toUpperCase()` methods to convert the string to lowercase and uppercase, respectively. Print both versions of the string.
 
-let str = prompt("Enter a string")
-let newMsg = str.toLowerCase().toUpperCase(  );
-console.log(newMsg);
+// let str = prompt("Enter a string")
+// let newMsg = str.toLowerCase();
+// let newStr = str.toUpperCase();
+// console.log(newMsg);
+// console.log(newStr);
+
+
+
+// Create a program that takes a string as input and two indices representing the start and end positions of the substring. Use the `slice()` method to extract the substring between the specified indices. Print the extracted substring.
+
+// let str = "mango"
+// console.log(str.slice(2,5));
+
+
+
+Write a script that prompts the user to input a string and a number. Use the `repeat()` method to repeat the original string the specified number of times. Print the repeated string.
+
+let str = "hello"
+console.log(str.repeat(2));
+
