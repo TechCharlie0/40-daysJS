47e1dacbd353f0c0deb8e9882803533ce019b85f
isha kandel <ishachttri@gmail.com>
2024-04-17 11:04:30 +0545

    upper ans lower

--- string.js
+++ string.js
@@ -1,5 +1,14 @@
 // Write a program that prompts the user to enter a string with leading and trailing whitespace. Use the `trim()` method to remove the whitespace, and then print the trimmed string.
 
 
-let newMsg = prompt("enter a string")
-console.log(newMsg.trim());
+// let newMsg = prompt("enter a string")
+// console.log(newMsg.trim());
+
+
+
+
+// Develop a script that prompts the user to input a string. Use the `toLowerCase()` and `toUpperCase()` methods to convert the string to lowercase and uppercase, respectively. Print both versions of the string.
+
+let str = prompt("Enter a string")
+let newMsg = str.toLowerCase().toUpperCase(  );
+console.log(newMsg);
