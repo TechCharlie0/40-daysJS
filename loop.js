32f80848ce0e966f624820aaa853d3a61ee92382
isha kandel <ishachttri@gmail.com>
2024-04-18 22:04:48 +0545

    student

--- loop.js
+++ loop.js
@@ -22,4 +22,10 @@
 // }
 
 
-let student = [ ["aman", 95], ["shradha", 94.5],[isha, 1], [utsav, 2]]
+let student = [ ["aman", 95], ["shradha", 94.5],["isha", 1], ["utsav", 2]];
+for (let i = 0; i<student.length; i++){
+for (let j = 0; j <student[i].length; j++) {
+console.log(student[i][j]) ;       
+    }
+
+}
