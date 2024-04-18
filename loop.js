341985e6e21c399a5eb34fe693e2264502f1883d
isha kandel <ishachttri@gmail.com>
2024-04-18 21:16:32 +0545

    fruits

--- loop.js
+++ loop.js
@@ -1,12 +1,22 @@
-let favDrama  = "a love so beautiful"
-let guess = prompt("enter my fav movie") ;
-while(( guess != favDrama )){
-    guess = prompt("wrong please try again");
+// movie using the while loop
+
+// let favDrama  = "a love so beautiful"
+// let guess = prompt("enter my fav movie") ;
+// while(( guess != favDrama )){
+//     guess = prompt("wrong please try again");
 
     
    
-}
-if (favDrama == guess) {
-    console.log("you are correct");
+// }
+// if (favDrama == guess) {
+//     console.log("you are correct");
+    
+// }
+
+// loops with arrays
+
+let fruits = ("mango", "apple", "papaya", "orange");
+for (let i = 0; i < fruits.length; i++) {
+    console.log(i , fruits[i]);
     
 }
