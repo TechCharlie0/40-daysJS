ea84fe64110310abd7de71109310fb35498f2076
isha kandel <ishachttri@gmail.com>
2024-04-18 21:08:33 +0545

    loop

---
+++ loop.js
@@ -0,0 +1,12 @@
+let favDrama  = "a love so beautiful"
+let guess = prompt("enter my fav movie") ;
+while(( guess != favDrama )){
+    guess = prompt("wrong please try again");
+
+    
+   
+}
+if (favDrama == guess) {
+    console.log("you are correct");
+    
+}
