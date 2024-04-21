496da8b24a1d5897003b9479f63d7feb56e0824f
isha kandel <ishachttri@gmail.com>
2024-04-21 21:13:14 +0545

    mass

--- first.js
+++ first.js
@@ -2,28 +2,48 @@
 
 // let day = 4;
 
-// switch(day){
-//     case 1:
-//         console.log("monday");
-//         break;
-//         case 2:
-//             console.log("tuesday");
-//             break;
-//             case 3:
-//                 console.log("wednesday");
-//                 break;
-//                 case 4:
-//                     console.log("thursday");
-//                     break;
-//                     case 5:
-//                         console.log("friday");
-//                         break;
-//                         case 6:
-//                             console.log("saturday");
-//                             break;
-//                              console.log("sunday");
-//                                 break;
+switch(day){
 
-// }
+    case 1:
+
+console.log("monday");
+
+break;
+       
+case 2:
+           
+console.log("tuesday");
+            
+break;
+            
+case 3:
+                
+console.log("wednesday");
+                
+break;
+                
+case 4:
+                   
+console.log("thursday");
+                   
+break;
+                   
+case 5:
+                        
+console.log("friday");
+                       
+break;
+                       
+case 6:
+                          
+console.log("saturday");
+                           
+break;
+                            
+console.log("sunday");
+                               
+break;
+
+}
 // Create a script that prompts the user to input their exam score as a percentage (e.g., 85 for 85%). Then, use a `switch` statement to convert the percentage to a letter grade (A, B, C, D, or F) based on the following scale: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
 
