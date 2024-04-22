6d7fb5b334c651264e0e2364323d00d7297cbba3
isha kandel <ishachttri@gmail.com>
2024-04-22 23:09:31 +0545

    string

--- question.js
+++ question.js
@@ -10,17 +10,17 @@ let place = "chitwan";
 console.log(place[2]);
 console.log(place.toUpperCase());
 
-// let arr = [10, 20, 30, 60];
+let arr = [10, 20, 30, 60];
 
-// console.log(arr[2]);
+console.log(arr[2]);
 
-// let isha = [10, "manisha", 20, "isha", 30];
-// isha[1]= "chake";
-// console.log(isha[0]);
-// console.log(isha[1]);
-// console.log(isha[2]);
-// console.log(isha[3]);
-// console.log(isha[4]);
+let isha = [10, "manisha", 20, "isha", 30];
+isha[1]= "chake";
+console.log(isha[0]);
+console.log(isha[1]);
+console.log(isha[2]);
+console.log(isha[3]);
+console.log(isha[4]);
 
 
 
