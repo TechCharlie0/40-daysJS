2845b515db45e122f50cec33995a8e4f02dd95a7
isha kandel <ishachttri@gmail.com>
2024-04-22 23:09:06 +0545

    sad

--- question.js
+++ question.js
@@ -6,7 +6,43 @@ console.log(`my name is: ${name} i live in: ${location}`);
 // my name is ... and i live in ...
  
 let place = "chitwan";
+
+console.log(place[2]);
 console.log(place.toUpperCase());
+
+// let arr = [10, 20, 30, 60];
+
+// console.log(arr[2]);
+
+// let isha = [10, "manisha", 20, "isha", 30];
+// isha[1]= "chake";
+// console.log(isha[0]);
+// console.log(isha[1]);
+// console.log(isha[2]);
+// console.log(isha[3]);
+// console.log(isha[4]);
+
+
+
+
+// push
+// pop
+// shift
+// unshift
+// slice
+// splice
+
+
+
+
+
+
+
+
+
+  
+
+
 
 
 
