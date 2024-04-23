f39fb338aed018b8138093149af0397229d89864
isha kandel <ishachttri@gmail.com>
2024-04-23 11:48:12 +0545

    unary

---
+++ template.js
@@ -0,0 +1,10 @@
+let firstName = "isha";
+let secondName = "kandel";
+let thirdName = "gaindakot";
+console.log(`my name is ${firstName} ${secondName} i live in ${thirdName} ` );
+
+
+let a = 10;
+let b = 12;
+console.log(a++);
+console.log(++a);
