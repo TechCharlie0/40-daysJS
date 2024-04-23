c5698cee85ad28a6a6ddad32704444034e9eb046
isha kandel <ishachttri@gmail.com>
2024-04-23 12:15:39 +0545

    age

--- day1/comparasion.js
+++ day1/comparasion.js
@@ -7,4 +7,48 @@ console.log(age <= 18);
 console.log(age != 18);
 
 
+let drive = 25;
+if (drive >=24) {
+    console.log("you can drive")
+    
+}
+ 
+//traffic color using if statement//
 
+let color = "green";
+
+if(color === "red"){
+console.log("you have to stop");
+}
+
+if(color === "yellow"){
+ console.log("go slow");
+ }
+
+ if(color === "green"){
+console.log(" you have to go");
+ }
+
+
+//else if statement//
+
+let ages = 14;
+if (ages >= 18) {
+    console.log("you can vote");
+    
+}
+else if(ages < 18){
+    console.log("you cannot vote");
+
+
+}
+
+        
+        
+     
+
+
+   
+
+
+ 
