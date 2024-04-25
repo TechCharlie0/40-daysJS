d025be7149063234d40bba80b47e3e864d8443db
isha kandel <ishachttri@gmail.com>
2024-04-25 17:58:40 +0545

    problem

--- day3/index.js
+++ day3/index.js
@@ -2,12 +2,25 @@
 // console.log(greet("hello"));
 // return func;
 
-function func() {
-  return function () {
-    return "Namaste";
-  };
-}
+// function func() {
+//   return function () {
+//     return "Namaste";
+//   };
+// }
 
-let greet = func();
-console.log(greet());
-console.log(greet());
+// let greet = func();
+// console.log(greet());
+// console.log(greet());
+
+//to reduce this problem higher oeder function is introduced
+
+function multipeFunction(funct, n) {
+  for (let i = 1; i <= n; i++) {
+    funct();
+  }
+}
+let greet = function () {
+  console.log("namaste");
+};
+
+multipeFunction(greet, 6);
