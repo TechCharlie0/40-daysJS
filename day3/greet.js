09146b1c1ca4b43e7d3a5d5a56d51fc3437f1d3d
isha kandel <ishachttri@gmail.com>
2024-04-25 22:29:42 +0545

    not good

--- day3/greet.js
+++ day3/greet.js
@@ -19,3 +19,46 @@ let newGreet = function () {
 };
 
 multipleFunct(newGreet, 7);
+
+//yei ma even ra odd check garna ko lagiii garxu
+
+function oddEven(n) {
+  if (typeof n === "number") {
+    // Check if n is a number
+    if (n % 2 === 0) {
+      console.log("even");
+    } else {
+      console.log("odd");
+    }
+  } else {
+    console.log("Invalid input"); // If n is not a number
+  }
+}
+
+function checkoddeven(n) {
+  if (typeof n == "number") {
+    if (n % 2 != 0) {
+      console.log("odd");
+    } else {
+      console.log("even");
+    }
+  } else {
+    console.log("invalid number");
+  }
+}
+
+//yeslai high orger batw grda
+
+function evenOddTest(request) {
+  if (request == "odd") {
+    return function (n) {
+      console.log(!(n % 2 == 0));
+    };
+  } else if (request == "even") {
+    return function (n) {
+      console.log(n % 2 == 0);
+    };
+  } else {
+    console.log("this is not a number");
+  }
+}
