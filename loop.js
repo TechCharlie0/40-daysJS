a40023d23f1c1207a16768df57a00c3f283d6afa
isha kandel <ishachttri@gmail.com>
2024-04-26 21:46:00 +0545

    ok

--- loop.js
+++ loop.js
@@ -1,14 +1,13 @@
 // movie using the while loop
 
-// let favDrama  = "a love so beautiful"
-// let guess = prompt("enter my fav movie") ;
-// while(( guess != favDrama )){
-//     guess = prompt("wrong please try again");
+let favDrama  = "a love so beautiful"
+let guess = prompt("enter my fav movie") ;
+while(( guess != favDrama )){
+    guess = prompt("wrong please try again");
 
     
    
-// }
-// if (favDrama == guess) {
+/// if (favDrama == guess) {
 //     console.log("you are correct");
     
 // }
