76f9c9fea6e8cedbbe6a359d8cb6e47e076ce075
isha kandel <ishachttri@gmail.com>
2024-04-27 22:48:46 +0545

    ok

--- day4/script.js
+++ day4/script.js
@@ -1,4 +1,29 @@
-let btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("this is dangerous site");
-};
+// let btns = document.querySelectorAll("button");
+
+// //yesma loop lauda for of launa parxa loop chai derai box athawa derai huda launi
+
+// for (btn of btns) {
+//   btn.onclick = sayHello;
+// }
+// console.dir(btns);
+
+// //yo arko tarikale milxa
+// btn.onclick = function () {
+//   alert("this is dangerous site");
+// };
+
+// //yo pani arko tarikako ho
+// function sayHello() {
+//   alert("hello");
+// }
+// btn.onclick = sayHello;
+
+let hell = document.querySelector("h2");
+// hell.onclick = function () {
+//   console.log("hello");
+// };
+
+function hells() {
+  console.log("oppps");
+}
+hell.onclick = hells;
