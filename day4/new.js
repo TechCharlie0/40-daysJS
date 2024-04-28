eb981d9ba0e92b5931834d970216c25e08b3bc55
isha kandel <ishachttri@gmail.com>
2024-04-28 21:36:23 +0545

    random color

--- day4/new.js
+++ day4/new.js
@@ -1,11 +1,11 @@
-let btn = document.querySelectorAll("button");
+// let btn = document.querySelectorAll("button");
 
-for (btns of btn) {
-  btns.addEventListener("click", myGod);
-}
-function myGod() {
-  console.log("ohgod");
-}
+// for (btns of btn) {
+//   btns.addEventListener("click", myGod);
+// }
+// function myGod() {
+//   console.log("ohgod");
+// }
 
 // user.onclick = function () {
 //   console.log("click it");
@@ -22,3 +22,23 @@ let char = document.querySelector("h1");
 char.addEventListener("click", function () {
   console.log("this is clicked");
 });
+
+let newBtn = document.querySelector("button");
+newBtn.addEventListener("click", function () {
+  let h1 = document.querySelector("h1");
+  let randomColor = getRamdomcolor();
+  h1.innerText = randomColor;
+
+  let div = document.querySelector("div");
+  div.style.backgroundColor = randomColor;
+  console.log("color updated");
+});
+
+function getRamdomcolor() {
+  let red = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+
+  let color = `rgb(${red}, ${blue}, ${green})`;
+  return color;
+}
