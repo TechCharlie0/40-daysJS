d38ef0487a0aa9d708feec0afa2a63cd86612a29
isha kandel <ishachttri@gmail.com>
2024-04-28 22:24:52 +0545

    ok

--- day4/new.js
+++ day4/new.js
@@ -16,29 +16,37 @@
 // }
 // btn.onclick = user;
 
-//eventlistener
+// //eventlistener
 
-let char = document.querySelector("h1");
-char.addEventListener("click", function () {
-  console.log("this is clicked");
+// let char = document.querySelector("h1");
+// char.addEventListener("click", function () {
+//   console.log("this is clicked");
+// });
+
+// let newBtn = document.querySelector("button");
+// newBtn.addEventListener("click", function () {
+//   let h1 = document.querySelector("h1");
+//   let randomColor = getRamdomcolor();
+//   h1.innerText = randomColor;
+
+//   let div = document.querySelector("div");
+//   div.style.backgroundColor = randomColor;
+//   console.log("color updated");
+// });
+
+// function getRamdomcolor() {
+//   let red = Math.floor(Math.random() * 255);
+//   let blue = Math.floor(Math.random() * 255);
+//   let green = Math.floor(Math.random() * 255);
+
+//   let color = `rgb(${red}, ${blue}, ${green})`;
+//   return color;
+// }
+
+//this eventhandler
+
+let news = document.querySelector("button");
+news.addEventListener("click", function () {
+  console.log(this.innerText);
+  this.style.backgroundColor = "red";
 });
-
-let newBtn = document.querySelector("button");
-newBtn.addEventListener("click", function () {
-  let h1 = document.querySelector("h1");
-  let randomColor = getRamdomcolor();
-  h1.innerText = randomColor;
-
-  let div = document.querySelector("div");
-  div.style.backgroundColor = randomColor;
-  console.log("color updated");
-});
-
-function getRamdomcolor() {
-  let red = Math.floor(Math.random() * 255);
-  let blue = Math.floor(Math.random() * 255);
-  let green = Math.floor(Math.random() * 255);
-
-  let color = `rgb(${red}, ${blue}, ${green})`;
-  return color;
-}
