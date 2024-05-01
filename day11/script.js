620b8ae18b4277a2ff8c8a19e046ead7cdbbaf43
isha kandel <ishachttri@gmail.com>
2024-05-01 23:08:28 +0545

    stack

--- day11/script.js
+++ day11/script.js
@@ -1,12 +1,28 @@
-function hello() {
-  console.log("hello how are you?");
-}
+// function hello() {
+//   console.log("hello how are you?");
+// }
 
-function demo() {
-  console.log("calling hello fun");
+// function demo() {
+//   console.log("calling hello fun");
 
-  hello();
+//   hello();
+// }
+// console.log("calling demo fun");
+// demo();
+// console.log("done");
+
+//visualizinf the call stack
+
+function one() {
+  return 1;
 }
-console.log("calling demo fun");
-demo();
-console.log("done");
+function two() {
+  return one() + one();
+}
+function three() {
+  return two() + two();
+}
+function four() {
+  let ans = three() + one();
+  console.log(ans);
+}
