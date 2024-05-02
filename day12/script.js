a80ff12c86f5d79d5a17750563a4beed91d17afd
isha kandel <ishachttri@gmail.com>
2024-05-02 17:15:08 +0545

    save

--- day12/script.js
+++ day12/script.js
@@ -1,15 +1,25 @@
-h1 = document.querySelector("h1");
-function changecolor(color, delay, nextcolorchange) {
-  setTimeout(() => {
-    h1.style.color = color;
-    if (nextcolorchange) nextcolorchange();
-  }, delay);
-}
+// h1 = document.querySelector("h1");
+// p = document.querySelector("p");
+// function changecolor(color, delay, nextcolorchange) {
+//   setTimeout(() => {
+//     h1.style.color = color;
+//     if (nextcolorchange) nextcolorchange();
+//   }, delay);
+// }
 
-changecolor("red", 1000, () => {
-  changecolor("orange", 1000, () => {
-    changecolor("blue", 1000, () => {
-      changecolor("yellow", 1000);
-    });
-  });
-});
+// changecolor("red", 1000, () => {
+//   changecolor("orange", 1000, () => {
+//     changecolor("blue", 1000, () => {
+//       changecolor("yellow", 1000);
+//     });
+//   });
+// });
+
+function saveToDatabase(data) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed > 4) {
+    console.log("your data was saved");
+  } else {
+    console.log("your dta was not saved");
+  }
+}
