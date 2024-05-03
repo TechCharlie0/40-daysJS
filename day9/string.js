95518d4001cc1e23079373fb7a1dfa6c3aed59b7
isha kandel <ishachttri@gmail.com>
2024-05-03 23:18:26 +0545

    new

--- day9/string.js
+++ day9/string.js
@@ -1,40 +1,25 @@
 // Write a program that prompts the user to enter a string with leading and trailing whitespace. Use the `trim()` method to remove the whitespace, and then print the trimmed string.
 
-
-// let newMsg = prompt("enter a string")
-// console.log(newMsg.trim());
-
-
-
+let newMsg = prompt("enter a string");
+console.log(newMsg.trim());
 
 // Develop a script that prompts the user to input a string. Use the `toLowerCase()` and `toUpperCase()` methods to convert the string to lowercase and uppercase, respectively. Print both versions of the string.
 
-let str = prompt("Enter a string")
+let str = prompt("Enter a string");
 let newMsg = str.toLowerCase();
 let newStr = str.toUpperCase();
 console.log(newMsg);
 console.log(newStr);
 
-
-
 // Create a program that takes a string as input and two indices representing the start and end positions of the substring. Use the `slice()` method to extract the substring between the specified indices. Print the extracted substring.
 
-// let str = "pemango"
-// console.log(str.slice(2,5));
-
-
-
-
-
+let str = "pemango";
+console.log(str.slice(2, 5));
 
 // Write a script that prompts the user to input a string and a number. Use the `repeat()` method to repeat the original string the specified number of times. Print the repeated string.
 
-// let str = "hello"
-// console.log(str.repeat(2));
+let strs = "hello";
+console.log(strs.repeat(2));
 
-
-
-let str = "hello"
+let str = "hello";
 console.log(str.indexOf());
-
-
