7e1201c290d6aae0514e4e1ccf0e6e3e92d5155e
isha kandel <ishachttri@gmail.com>
2024-05-03 23:10:04 +0545

    add

--- day12/script.js
+++ day12/script.js
@@ -1,19 +1,21 @@
-// h1 = document.querySelector("h1");
-// p = document.querySelector("p");
-// function changecolor(color, delay, nextcolorchange) {
-//   setTimeout(() => {
-//     h1.style.color = color;
-//     if (nextcolorchange) nextcolorchange();
-//   }, delay);
-// }
+h1 = document.querySelector("h1");
+p = document.querySelector("p");
+function changecolor(color, delay, nextcolorchange) {
+  setTimeout(() => {
+    h1.style.color = color;
+    if (nextcolorchange) nextcolorchange();
+  }, delay);
+}
 
-// changecolor("red", 1000, () => {
-//   changecolor("orange", 1000, () => {
-//     changecolor("blue", 1000, () => {
-//       changecolor("yellow", 1000);
-//     });
-//   });
-// });
+changecolor("red", 1000, () => {
+  changecolor("orange", 1000, () => {
+    changecolor("blue", 1000, () => {
+      changecolor("yellow", 1000, () => {
+        changecolor("purple", 1000)
+      });
+    });
+  });
+});
 
 // function saveToDatabase(data, sucess, failure) {
 //   let internetSpeed = Math.floor(Math.random() * 10) + 1;
@@ -56,23 +58,31 @@
 //asynchronous data
 
 function saveToDatabase(data) {
-  return new Promise((resolve, reject) => {
-    let internetSpeed = Math.floor(Math.random() * 10) + 1;
-    if (internetSpeed > 4) {
-      resolve("sucess");
-    } else {
-      reject("failure");
-    }
-  });
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if (internetSpeed > 4) {
+            resolve("sucess");
+        } else {
+            reject("failure");
+        }
+    });
 }
 
 //then() & catch()
 
-let request = saveToDatabase("apna college");
-request
-  .then(() => {
-    console.log("promess resolved");
-  })
-  .catch(() => {
-    console.log("promise rejected");
-  });
+saveToDatabase("apna college");
+ .then(() => {
+    console.log("promess resolved1");
+    return saveToDatabase("hello");
+     
+    
+    })
+    .then(() => {
+        console.log("sucess")
+        
+})
+
+
+    .catch(() => {
+        console.log("promise rejected");
+    });
