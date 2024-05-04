cd62150f3521bc0d96151968a0a70f042c8ec462
isha kandel <ishachttri@gmail.com>
2024-05-04 22:26:05 +0545

    h\getnum

--- day13/script.js
+++ day13/script.js
@@ -1,17 +1,43 @@
 //async and  await
 
-async function greet() {
-  // throw"some randowm error";
-  return "hello"; // return a promise
+// async function greet() {
+//   throw"some randowm error";
+//   return "hello"; // return a promise
+// }
+
+// // let hello = async () => ();  //return a promise
+
+// greet();
+// .then ((result) =>{
+//     console.log(resolved);
+//     console.log("result was:" result);
+// })
+// .catch((err) =>{
+//     console.log(rejected);
+// })
+
+// function getNum() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let num = Math.floor(Math.random() * 10) + 1;
+//       resolve(num);
+//     }, 1000);
+//   });
+// }
+function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      resolve(num);
+    }, 1000);
+  });
 }
 
-// let hello = async () => ();  //return a promise
-
-greet();
-.then ((result) =>{
-    console.log(resolved);
-    console.log("result was:" result);
-})
-.catch((err) =>{
-    console.log(rejected);
-})
+// Now you can call the getNum() function
+getNum()
+  .then((num) => {
+    console.log("Random number:", num);
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
