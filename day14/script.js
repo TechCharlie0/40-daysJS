80a0992bc6cf28c93a1cd58ce9ddc0305b9b229f
isha kandel <ishachttri@gmail.com>
2024-05-06 23:03:47 +0545

    facts

--- day14/script.js
+++ day14/script.js
@@ -3,7 +3,7 @@ let url = "https://catfact.ninja/fact";
 async function getFacts() {
   try {
     let res = await axios.get(url);
-    console.log(res);
+    console.log(res.data);
   } catch (e) {
     console.log("error -", e);
   }
