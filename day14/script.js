688559d43c25421b650e85c907187cba5b4c45ac
isha kandel <ishachttri@gmail.com>
2024-05-06 23:13:00 +0545

    facts

--- day14/script.js
+++ day14/script.js
@@ -1,10 +1,19 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+  let facts = await getFacts();
+  console.log(facts.data);
+});
+
 let url = "https://catfact.ninja/fact";
 
 async function getFacts() {
   try {
     let res = await axios.get(url);
-    console.log(res.data);
+    return res.data.fact;
+    // console.log(res.data.fact);
   } catch (e) {
     console.log("error -", e);
+    return "No fact found";
   }
 }
