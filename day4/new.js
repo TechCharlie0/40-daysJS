ad74bc8ca69dce78b7556e10e0ef8156a36d3c0f
isha kandel <ishachttri@gmail.com>
2024-05-07 22:45:01 +0545

    ok

--- day4/new.js
+++ day4/new.js
@@ -11,17 +11,17 @@ user.onclick = function () {
   console.log("click it");
 };
 
-// function user() {
-//   console.log("you click me");
-// }
-// btn.onclick = user;
+function user() {
+  console.log("you click me");
+}
+btn.onclick = user;
 
-// //eventlistener
+//eventlistener
 
-// let char = document.querySelector("h1");
-// char.addEventListener("click", function () {
-//   console.log("this is clicked");
-// });
+let char = document.querySelector("h1");
+char.addEventListener("click", function () {
+  console.log("this is clicked");
+});
 
 // let newBtn = document.querySelector("button");
 // newBtn.addEventListener("click", function () {
