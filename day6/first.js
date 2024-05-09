16dc7e48cff0d763cd626a8cf596449c8eab81b9
isha kandel <ishachttri@gmail.com>
2024-05-09 22:28:56 +0545

    ok

--- day6/first.js
+++ day6/first.js
@@ -1,49 +1,40 @@
 // Write a program that takes a number representing a day of the week (1 for Monday, 2 for Tuesday, etc.) and prints the name of the corresponding day.
 
-// let day = 4;
+let day = 4;
 
-// switch(day){
+switch (day) {
+  case 1:
+    console.log("monday");
 
-//     case 1:
+    break;
 
-// console.log("monday");
+  case 2:
+    console.log("tuesday");
 
-// break;
-       
-// case 2:
-           
-// console.log("tuesday");
-            
-// break;
-            
-// case 3:
-                
-// console.log("wednesday");
-                
-// break;
-                
-// case 4:
-                   
-// console.log("thursday");
-                   
-// break;
-                   
-// case 5:
-                        
-// console.log("friday");
-                       
-// break;
-                       
-// case 6:
-                          
-// console.log("saturday");
-                           
-// break;
-                            
-// console.log("sunday");
-                               
-// break;
+    break;
 
-// }
+  case 3:
+    console.log("wednesday");
+
+    break;
+
+  case 4:
+    console.log("thursday");
+
+    break;
+
+  case 5:
+    console.log("friday");
+
+    break;
+
+  case 6:
+    console.log("saturday");
+
+    break;
+
+    console.log("sunday");
+
+    break;
+}
 // // Create a script that prompts the user to input their exam score as a percentage (e.g., 85 for 85%). Then, use a `switch` statement to convert the percentage to a letter grade (A, B, C, D, or F) based on the following scale: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
-
