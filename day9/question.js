6ebd6ca6f8cb4522630428e01182036274870887
isha kandel <ishachttri@gmail.com>
2024-05-10 22:02:46 +0545

    ok

--- day9/question.js
+++ day9/question.js
@@ -5,23 +5,23 @@ console.log(`my name is: ${name} i live in: ${location}`);
 
 // // my name is ... and i live in ...
 
-// let place = "chitwan";
+let place = "chitwan";
 
-// console.log(place[2]);
-// console.log(place.toUpperCase());
+console.log(place[2]);
+console.log(place.toUpperCase());
 
-// let arr = [10, 20, 30, 60];
+let arr = [10, 20, 30, 60];
 
-// console.log(arr[2]);
+console.log(arr[2]);
 
-// let isha = [10, "manisha", 20, "isha", 30];
-// isha[1]= "chake";
+let isha = [10, "manisha", 20, "isha", 30];
+isha[1] = "chake";
 
-// console.log(isha[0]);
-// console.log(isha[1][2]);
-// console.log(isha[2]);
-// console.log(isha[3]);
-// console.log(isha[4]);
+console.log(isha[0]);
+console.log(isha[1][2]);
+console.log(isha[2]);
+console.log(isha[3]);
+console.log(isha[4]);
 
 // // push
 // // pop
