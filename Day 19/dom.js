1b2e80971d12313136284473c4298f24720f9583
isha kandel <ishachttri@gmail.com>
2024-06-25 21:48:25 +0545

    using properties and methods

---
+++ Day 19/dom.js
@@ -0,0 +1,9 @@
+//using properties and method
+//innerText:shows the visible text contained in a node
+//textContent:shows all the full text
+//innerHTML:shoes the full marksup
+
+let innerTxt = document.querySelector("p");
+console.log(innerTxt.innerText);
+console.log(innerTxt.textContent);
+console.log(innerTxt.innerHTML);
