4525bb548bed9908cbb3ed3d192dfb91dadcda14
isha kandel <ishachttri@gmail.com>
2024-06-26 16:15:17 +0545

    insert adjacent

--- Day 19/append.js
+++ Day 19/append.js
@@ -7,4 +7,16 @@ console.dir(newP);
 
 //appendchilds(element):to add the leement in last of the body
 let body = document.querySelector("body");
-body.appendChild(newP);
+console.log(body.appendChild(newP));
+
+//append:to change the element/string/text
+console.log(newP.append("this is new text"));
+
+//prepent:add in the starting
+console.log(body.prepend(newP));
+
+//insert adjacent(where, element)
+let btn = document.createElement("button");
+console.log((btn.innerHTML = "new button!"));
+let p = document.querySelector("p");
+p.insertAdjacentElement("beforebegin", btn);
