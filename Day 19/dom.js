b791996931eeb28446eadaf5f25093788fd96685
isha kandel <ishachttri@gmail.com>
2024-06-26 15:42:54 +0545

    classlist

--- Day 19/dom.js
+++ Day 19/dom.js
@@ -11,3 +11,37 @@ console.log(innerTxt.innerHTML);
 //manipulating attributes
 //obj.getAttribute(attr)
 //obj.setAttribute(attr,val)
+
+let img = document.querySelector("img");
+console.log(img.getAttribute("class"));
+console.log(img.setAttribute("class", "spidermanImg"));
+console.log(img.setAttribute("src", "./img2.jpg"));
+
+//manipulating style
+//style property
+
+let styl = document.querySelector("h1");
+console.log((styl.style.color = "purple"));
+console.log((styl.style.backgroundColor = "Pink"));
+
+let links = document.querySelectorAll(".box a");
+for (let i = 0; i < links.length; i++) {
+  links[i].style.color = "orange";
+}
+
+//using classList
+//obj.classList
+let cls = document.querySelector("h1");
+console.log(cls.classList);
+
+//to add new classes
+console.log(cls.classList.add("abc"));
+
+//to remove a new class
+console.log(cls.classList.remove("abc"));
+
+//to check if class exists
+console.log(cls.classList.contains("abc"));
+
+//to toggle between add and remove
+console.log(cls.classList.toggle("abc"));
