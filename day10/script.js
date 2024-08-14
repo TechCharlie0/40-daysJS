f7c120dbc72abe39cd8df4d1c5b777752039714e
isha kandel <ishachttri@gmail.com>
2024-08-14 23:27:25 +0545

    done

---
+++ day10/script.js
@@ -0,0 +1,29 @@
+let inp = document.querySelector("input");
+let btns = document.querySelector("button");
+let ul = document.querySelector("ul");
+btns.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = inp.value;
+  ul.appendChild(item);
+  inp.value = "";
+
+  let delbtn = document.createElement("button");
+  delbtn.innerText = "delete";
+  item.appendChild(delbtn);
+});
+
+ul.addEventListener("click", function (event) {
+  if (event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement;
+    listItem.remove();
+  }
+});
+
+let delbtns = document.querySelectorAll("button");
+for (delbtn of delbtns) {
+  delbtn.addEventListener("click", function () {
+    let par = this.parentElement;
+    console.log(par);
+    par.remove();
+  });
+}
