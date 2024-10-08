db3b28ce6a5e2c5949cb33e5126b1555bc0fece5
isha kandel <ishachttri@gmail.com>
2024-10-08 14:02:50 +0545

    resolve reject

--- day 28/script.js
+++ day 28/script.js
@@ -1,3 +1,4 @@
+//async
 async function greet() {
   //promise return
   return "hello world";
@@ -9,3 +10,21 @@ greet()
   .catch((err) => {
     console.log("promise was rejected with err:'.err");
   });
+let isha = async () => {
+  return "hi ishu how are you?";
+};
+
+//await
+function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      console.log(num);
+    }, 1000);
+  });
+}
+async function demo() {
+  await getNum();
+  await getNum();
+  await getNum();
+}
